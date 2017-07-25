a3a6b7d7eadee3d26a3ac12d38bcb87e1d47002d
JurickJoling <jurick.joling0906@outlook.com>
2017-07-25 15:27:04 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,117 @@
+// Setup basic express server
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
+
+//Including dependency
+var Sequelize = require('sequelize');
+ 
+//Setting up the config
+var sequelize = new Sequelize('Chatroom', 'root', 'root', {
+    host: 'localhost',
+    port: 3306,
+    dialect: 'mysql',
+});
+
+var test = sequelize.authenticate()
+    .then(function () {
+        console.log("CONNECTED! ");
+    })
+    .catch(function (err) {
+        console.log("SOMETHING DONE GOOFED");
+    })
+    .done();
+
+//Create Item Table Structure
+var Item = sequelize.define('Item', {
+    id:{
+      type: Sequelize.INTEGER(11),
+      allowNull:     false,
+      primaryKey:    true,
+      autoIncrement: true
+    }, 
+    username:Sequelize.STRING
+    
+});
+ 
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
+
+// Routing
+app.use(express.static(__dirname + '/public'));
+
+// Chatroom
+
+var numUsers = 0;
+
+io.on('connection', function (socket) {
+  var addedUser = false;
+
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', function (data) {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
+  });
+
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', function (username) {
+    if (addedUser) return;
+
+    console.log(username);
+
+    sequelize.sync()
+      .then(() => Item.create({
+        username: username,
+      }))
+    .then(jane => {
+      console.log(jane.get({
+        plain: true
+      }));
+    });
+    
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', function () {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', function () {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function () {
+    if (addedUser) {
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
+});
